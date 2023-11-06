2ff208a81123b42e1661864d58c2fd95d4eca0bd
Ivan <0675361905vi@gmail.com>
2023-11-06 11:54:39 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,25 +1,37 @@
 
 // Функція рандомної зміни кольору 
 function getRandomHexColor() {
-    return #${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)};
-    }
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+};
+  
 
     const body = document.querySelector('.js-body');
     const buttonStart = document.querySelector('.js-buttonStart');
     const buttonStop = document.querySelector('.js-buttonStop');
-    let timerId = null;
+    let animationFrameId = null;
     
 
-    buttonStart.addEventListener("click", () => {
-    timerId = setInterval(() => {
-        body.style.backgroundColor=getRandomHexColor();
-    }, 1000);
-    buttonStart.setAttribute("disabled", "disabled");
-    });
+// Функція для анімації зміни кольору
+function animateColorChange() {
+    body.style.backgroundColor = getRandomHexColor();
+    animationFrameId = requestAnimationFrame(animateColorChange);
+}
 
+// Кнопка старт зміни кольору
+buttonStart.addEventListener("click", () => {
+    if (!animationFrameId) {
+        animateColorChange();
+        buttonStart.setAttribute("disabled", "disabled");
+        buttonStop.removeAttribute("disabled");
+    }
+});
 
-    buttonStop.addEventListener("click", () => {
-    clearInterval(timerId);
-    buttonStop.removeAttribute("disabled");
-    });
-
+// Кнопка стоп зміни кольору 
+buttonStop.addEventListener("click", () => {
+    if (animationFrameId) {
+        cancelAnimationFrame(animationFrameId);
+        animationFrameId = null;
+        buttonStop.setAttribute("disabled", "disabled");
+        buttonStart.removeAttribute("disabled");
+    }
+});
