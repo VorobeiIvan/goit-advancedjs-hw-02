bd9b256a16a2859644547a415f41cc00e6308fc5
Ivan <0675361905vi@gmail.com>
2023-11-06 13:39:06 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,32 +1,27 @@
-// Функція рандомної зміни кольору 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
-};
-  
-    const body = document.querySelector('.js-body');
-    const buttonStart = document.querySelector('.js-buttonStart');
-    const buttonStop = document.querySelector('.js-buttonStop');
-    let animation = null;
+}
+const body = document.querySelector('.js-body');
+const buttonStart = document.querySelector('.js-buttonStart');
+const buttonStop = document.querySelector('.js-buttonStop');
+let animation = null;
 
-// Функція для анімації зміни кольору
-function colorChange() {
+function animateColorChange() {
     body.style.backgroundColor = getRandomHexColor();
+    animation = requestAnimationFrame(animateColorChange);
 }
 
-// Кнопка старт зміни кольору
 buttonStart.addEventListener("click", () => {
     if (!animation) {
-        animation = setInterval(colorChange, 1000);
+        animateColorChange();
         buttonStart.setAttribute("disabled", "disabled");
         buttonStop.removeAttribute("disabled");
-        colorChange();
     }
 });
 
-// Кнопка стоп зміни кольору 
 buttonStop.addEventListener("click", () => {
     if (animation) {
-        cancelAnimation(animation);
+        cancelAnimationFrame(animation);
         animation = null;
         buttonStop.setAttribute("disabled", "disabled");
         buttonStart.removeAttribute("disabled");
