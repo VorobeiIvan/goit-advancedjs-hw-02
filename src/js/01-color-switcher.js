23088275f662294b0ee4a31f8a3fa52fd6f47ca4
VorobeiIvan <0675361905vi@gmail.com>
2023-11-06 19:17:06 +0200

    change to slower animation

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,29 +1,32 @@
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
 }
+
 const body = document.querySelector('.js-body');
 const buttonStart = document.querySelector('.js-buttonStart');
 const buttonStop = document.querySelector('.js-buttonStop');
 let animation = null;
 
 function animateColorChange() {
-    body.style.backgroundColor = getRandomHexColor();
-    animation = requestAnimationFrame(animateColorChange);
+  body.style.backgroundColor = getRandomHexColor();
 }
 
-buttonStart.addEventListener("click", () => {
-    if (!animation) {
-        animateColorChange();
-        buttonStart.setAttribute("disabled", "disabled");
-        buttonStop.removeAttribute("disabled");
-    }
+buttonStart.addEventListener('click', () => {
+  if (!animation) {
+    animation = setInterval(animateColorChange, 500);
+    buttonStart.setAttribute('disabled', 'disabled');
+    buttonStop.removeAttribute('disabled');
+    animateColorChange();
+  }
 });
 
-buttonStop.addEventListener("click", () => {
-    if (animation) {
-        cancelAnimationFrame(animation);
-        animation = null;
-        buttonStop.setAttribute("disabled", "disabled");
-        buttonStart.removeAttribute("disabled");
-    }
+buttonStop.addEventListener('click', () => {
+  if (animation) {
+    clearInterval(animation);
+    animation = null;
+    buttonStop.setAttribute('disabled', 'disabled');
+    buttonStart.removeAttribute('disabled');
+  }
 });
