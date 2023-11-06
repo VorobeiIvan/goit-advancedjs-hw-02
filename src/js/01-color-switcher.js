b969c9b9f957ce7db883440bb36235954d1445da
Ivan <0675361905vi@gmail.com>
2023-11-06 11:39:40 +0200

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
 
+// Функція рандомної зміни кольору 
+function getRandomHexColor() {
+    return #${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)};
+    }
+
+    const body = document.querySelector('.js-body');
+    const buttonStart = document.querySelector('.js-buttonStart');
+    const buttonStop = document.querySelector('.js-buttonStop');
+    let timerId = null;
+    
+
+    buttonStart.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        body.style.backgroundColor=getRandomHexColor();
+    }, 1000);
+    buttonStart.setAttribute("disabled", "disabled");
+    });
+
+
+    buttonStop.addEventListener("click", () => {
+    clearInterval(timerId);
+    buttonStop.removeAttribute("disabled");
+    });
+
